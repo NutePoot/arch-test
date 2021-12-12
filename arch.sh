fa93fc241a0a613906dceb787a9776602ba08842
NutePoot <68525151+NutePoot@users.noreply.github.com>
2021-12-12 16:36:03 +1100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,11 +3,16 @@
 #clear tty 
 clear
   
-#disk config
-gdisk /dev/sda
-o
-n
-2048
-+512M
-ef00
-p
+#partitioning
+cfdisk
+
+#formatting
+mkfs.fat -F32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+
+#mounting
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+swapon /dev/sda2
