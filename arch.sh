ad45c1dfed0f99a8b8f6a39b726e49c60eeaee31
NutePoot <68525151+NutePoot@users.noreply.github.com>
2021-12-12 16:07:24 +1100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,8 +4,8 @@
   clear
   
 #disk config
-	print "Disk Configuration"
- select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
+print "Disk Configuration"
+select ENTRY in $(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd");
 do
     DISK=$ENTRY
     print "Installing Arch Linux on $DISK."
