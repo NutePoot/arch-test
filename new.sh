87b8cf8a444bb2865dc19011419da3ec66c44866
NutePoot <68525151+NutePoot@users.noreply.github.com>
2021-12-12 15:29:51 +1100

    Update new.sh

--- new.sh
+++ new.sh
@@ -2,3 +2,30 @@
 
 # Cleaning the TTY.
 clear
+
+# Pretty print (function).
+print () {
+    echo -e "\e[1m\e[93m[ \e[92m•\e[93m ] \e[4m$1\e[0m"
+}
+
+# Selecting a kernel to install (function). 
+kernel_selector () {
+    print "List of kernels:"
+    print "1) Stable: Vanilla Linux kernel with a few specific Arch Linux patches applied"
+    print "2) Hardened: A security-focused Linux kernel"
+    print "3) LTS: Long-term support (LTS) Linux kernel"
+    print "4) Zen: A Linux kernel optimized for desktop usage"
+    read -r -p "Insert the number of the corresponding kernel: " choice
+    case $choice in
+        1 ) kernel="linux"
+            ;;
+        2 ) kernel="linux-hardened"
+            ;;
+        3 ) kernel="linux-lts"
+            ;;
+        4 ) kernel="linux-zen"
+            ;;
+        * ) print "You did not enter a valid selection."
+            kernel_selector
+    esac
+}
